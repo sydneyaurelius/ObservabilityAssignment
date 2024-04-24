93b75f033b8b8e1ff569af6a7aa49497b36f9ba5
Sydney Aurelius <sydneyaurelius@gmail.com>
2024-04-23 22:47:37 -0400

    Edit tracing

--- tracing.js
+++ tracing.js
@@ -1,6 +1,6 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { JaegerExporter } = require('@opentelemetry/exporter-jaeger'); // Update import
+const { JaegerExporter } = require('@opentelemetry/jaeger-exporter');
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
@@ -11,7 +11,7 @@ const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 //Exporter
 module.exports = (serviceName) => {
-   const exporter = new JaegerExporter({ serviceName }); // Update exporter instantiation
+   const exporter = new JaegerExporter();
    const provider = new NodeTracerProvider({
        resource: new Resource({
            [SemanticResourceAttributes.SERVICE_NAME]: 'todo-service',
@@ -27,5 +27,5 @@ module.exports = (serviceName) => {
        ],
        tracerProvider: provider,
    });
-   return trace.getTracer('todo-service');
+   return trace.getTracer(serviceName);
 };
