f68e723b4df490a83eefc42dbc8404155a0833b6
Sydney Aurelius <sydneyaurelius@gmail.com>
2024-04-23 22:48:59 -0400

    Edit tracing

--- tracing.js
+++ tracing.js
@@ -1,6 +1,6 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { JaegerExporter } = require('@opentelemetry/jaeger-exporter');
+const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
